8b9d7053e15b4a230ed5b23e9da3fde71fb387bd
Yaroslav <101300838+uagmap@users.noreply.github.com>
2026-02-27 11:44:22 +0300

    Add files via upload

---
+++ Microchip_Atmega2560/MemoryCrane/eeprom.h
@@ -0,0 +1,27 @@
+/*
+ * eeprom.h
+ *
+ * Created: 03.12.2023 17:32:44
+ *  Author: yarma
+ */ 
+
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+#include <avr/io.h>
+
+#define EEPROM_start_address 0x0000
+
+
+// function prototypes
+void eeprom_write(uint16_t address, uint8_t data);
+uint8_t eeprom_read(uint16_t address);
+
+// Mode input definitions
+
+
+//global variables
+
+
+#endif /* EEPROM_H_ */
